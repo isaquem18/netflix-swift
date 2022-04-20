4e9ae3144b6d3a7a4f46eb5016181831fe5815ec
Isaque Maninde Moura Patricio <isaquem18@gmail.com>
2022-04-20 12:27:20 -0300

    code base

---
+++ netflix-swift/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  netflix-swift
+//
+//  Created by Isaque Moura on 20/04/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, HOME!")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+            .previewLayout(.device)
+    }
+}
---
+++ netflix-swift/netflix_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflix_swiftApp.swift
+//  netflix-swift
+//
+//  Created by Isaque Moura on 20/04/22.
+//
+
+import SwiftUI
+
+@main
+struct netflix_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
